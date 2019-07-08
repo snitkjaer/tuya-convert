b0fbc0c927f3472cb91bea6010793175cfbab17a
Bo Snitkjær Nielsen <snitkjaer@gmail.com>
2019-07-08 14:22:43 +0200

    Updatede install_prereq.sh to fix error on Buster
    Error in E: Repository 'http://raspbian.raspberrypi.org/raspbian buster InRelease' changed its 'Suite' value from 'testing' to 'stable'
    
    sudo apt get update -y

--- install_prereq.sh
+++ install_prereq.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y dnsmasq hostapd screen curl python3-pip python3-setuptools python3-wheel mosquitto haveged net-tools
+sudo apt get update -y
+sudo apt get upgrade -y
+sudo apt get install -y dnsmasq hostapd screen curl python3-pip python3-setuptools python3-wheel mosquitto haveged net-tools
 
 sudo pip3 install paho-mqtt pyaes tornado
 
